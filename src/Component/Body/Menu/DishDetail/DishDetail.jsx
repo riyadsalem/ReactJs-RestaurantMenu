a10afd47590ce2d2f864fbde0670e018f58e1d37
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-11-04 18:43:50 +0200

    DishDetail Component Part 1

---
+++ src/Component/Body/Menu/DishDetail/DishDetail.jsx
@@ -0,0 +1,11 @@
+import "./DishDetail.css";
+
+const DishDetail = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default DishDetail;
--- src/Component/Body/Menu/Menu.jsx
+++ src/Component/Body/Menu/Menu.jsx
@@ -2,20 +2,36 @@ import "./Menu.css";
 import React, { Component } from "react";
 import MenuItem from "./MenuItem/MenuItem";
 import DISHES from "../../../data/dishes";
+import DishDetail from "./DishDetail/DishDetail";
 
 export default class Menu extends Component {
-  state = { dishes: DISHES };
+  state = {
+    dishes: DISHES,
+    selectedDish: null,
+  };
+
+  onDishSelect = (dish) => {
+    this.setState({ selectedDish: dish });
+  };
+
   render() {
     const menu = this.state.dishes.map((item) => {
-      return <MenuItem key={item.id} dish={item} />;
+      return (
+        <MenuItem key={item.id} dish={item} DishSelect={this.onDishSelect} />
+      );
     });
+
+    let dishDetail = null;
+    if (this.state.selectedDish != null) {
+      dishDetail = <DishDetail dish={this.state.selectedDish} />;
+    }
 
     return (
       <>
         <div className="container">
           <div className="row">
             <div className="col-6">{menu}</div>
-            <div className="col-6">Col 6</div>
+            <div className="col-6">{dishDetail}</div>
           </div>
         </div>
       </>
